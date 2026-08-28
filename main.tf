95a224b523a3783383d3c09f76e04010331429fe
Kavitha <62429717+iamkavitha@users.noreply.github.com>
2026-08-28 16:17:02 +0530

    Rename aws_instance resource from 'server' to 'foo'

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
     region = "us-east-1"  
 }
 
-resource "aws_instance" "server" {
+resource "aws_instance" "foo" {
   ami           = "ami-0332d564d76dbd8d6" 
   instance_type = "t3.small"
   tags = {
