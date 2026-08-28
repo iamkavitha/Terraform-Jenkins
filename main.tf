5af77801cb39341f33be67b7bbd6417e1ef0a4c6
Kavitha <62429717+iamkavitha@users.noreply.github.com>
2026-08-28 16:03:34 +0530

    Rename aws_instance and update its configuration

--- main.tf
+++ main.tf
@@ -2,10 +2,10 @@ provider "aws" {
     region = "us-east-1"  
 }
 
-resource "aws_instance" "foo" {
-  ami           = "ami-05fa00d4c63e32376" # us-west-2
-  instance_type = "t2.micro"
+resource "aws_instance" "server" {
+  ami           = "ami-0332d564d76dbd8d6" 
+  instance_type = "t3.micro"
   tags = {
-      Name = "TF-Instance"
+      Name = "Terraform-Instance"
   }
 }
