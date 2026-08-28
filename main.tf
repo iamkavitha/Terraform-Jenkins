a5f96afaf27785105cac0d1b192ef6847d98bfcc
Kavitha <62429717+iamkavitha@users.noreply.github.com>
2026-08-28 16:14:25 +0530

    Change instance type from t3.micro to t3.small

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 resource "aws_instance" "server" {
   ami           = "ami-0332d564d76dbd8d6" 
-  instance_type = "t3.micro"
+  instance_type = "t3.small"
   tags = {
       Name = "Terraform-Instance"
   }
